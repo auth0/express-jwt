d335c70b7055c014f23463396907c14e232d0e72
José F. Romaniello <jfromaniello@gmail.com>
2015-03-06 08:28:41 -0300

    fix issue decoding JWT when the payload is a string

---
+++ test/string_token.test.js
@@ -0,0 +1,22 @@
+var jwt = require('jsonwebtoken');
+var assert = require('assert');
+
+var expressjwt = require('../lib');
+var UnauthorizedError = require('../lib/errors/UnauthorizedError');
+
+describe('string tokens', function () {
+  var req = {};
+  var res = {};
+
+  it('should work with a valid string token', function() {
+    var secret = 'shhhhhh';
+    var token = jwt.sign('foo', secret);
+
+    req.headers = {};
+    req.headers.authorization = 'Bearer ' + token;
+    expressjwt({secret: secret})(req, res, function() {
+      assert.equal('foo', req.user);
+    });
+  });
+
+});
