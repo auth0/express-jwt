auth0/express-jwt

commit bbd3606ce68da2602733d6e4ac32564570753ca1

requestProperty support for nested properties
This reverts the behavior to what v6 used to do.

src/index.ts
@@ -1,6 +1,8 @@
-import * as jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import * as express from 'express';
 import { unless } from 'express-unless';
+import set from 'lodash.set';
+
 import { UnauthorizedError } from './errors/UnauthorizedError';
 
 /**
@@ -123,7 +125,7 @@ export const expressjwt = (options: Params) => {
           .map(header => header.trim().toLowerCase())
           .includes('authorization');
         if (hasAuthInAccessControl) {
-          return next();
+          return setImmediate(next);
         }
       }
 
@@ -185,10 +187,10 @@ export const expressjwt = (options: Params) => {
       }
 
       const request = req as Request<jwt.JwtPayload | string>;
-      request[requestProperty] = decodedToken.payload;
-      next();
+      set(request, requestProperty, decodedToken.payload);
+      setImmediate(next);
     } catch (err) {
-      return next(err);
+      setImmediate(next, err);
     }
   };
 

test/jwt.test.ts
@@ -2,7 +2,7 @@
 import * as jwt from 'jsonwebtoken';
 import * as express from 'express';
 import { expressjwt, UnauthorizedError, Request, GetVerificationKey } from '../src';
-import * as assert from 'assert';
+import assert from 'assert';
 
 
 describe('failure tests', function () {
@@ -285,6 +285,21 @@ describe('work tests', function () {
     req.headers.authorization = 'Bearer ' + token;
     expressjwt({ secret: secret, algorithms: ['HS256'] })(req, res, function () {
       assert.equal(req.auth.foo, 'bar');
+      done();
+    });
+  });
+
+  it('should work with custom and nested request property', function (done) {
+    const secret = 'shhhhhh';
+    const token = jwt.sign({ foo: 'bar' }, secret);
+    const req = {} as Request;
+    const res = {} as express.Response;
+    const requestProperty = 'auth.payload';
+
+    req.headers = {};
+    req.headers.authorization = 'Bearer ' + token;
+    expressjwt({ secret: secret, algorithms: ['HS256'], requestProperty })(req, res, function () {
+      assert.equal(req.auth.payload.foo, 'bar');
       done();
     });
   });

commit c76ac3968281aed871c9c74306a912018803fd62

Fix: Remove signature change of middleware
The middleware must be void, not return things

Reverts breakage from bbd3606ce68da2602733d6e4ac32564570753ca1

src/index.ts
@@ -125,7 +125,8 @@ export const expressjwt = (options: Params) => {
           .map(header => header.trim().toLowerCase())
           .includes('authorization');
         if (hasAuthInAccessControl) {
-          return setImmediate(next);
+          setImmediate(next);
+          return;
         }
       }
 